e98d34e3faa6868c5d8c457e64ee5601fe12ac82
kimDDong <kdh2637@naver.com>
2016-11-22 19:46:33 +0900

    mob_ver4.c
    adding potion system

--- mob.c
+++ mob.c
@@ -3,7 +3,7 @@
 		printf("[몬스터 체력 : %d / %d]\n",mobhp,mobmhp);
 		printf("[나의 체력 : %d / %d]\n",hp,mhp);
 		printf("[포션 개수 : %d]\n",potion);
-		printf("[1]공격\n[2]도망\n[3]포션\n"); if(job >= 1){printf("[4]스킬\n");}
+		printf("[1]공격\n[2]도망\n[3]포션\n[4]스킬\n"); if(job >= 1){printf("[4]스킬\n");}
 		scanf("%d",&choose);
 }
 if(choose == 1)
@@ -35,3 +35,22 @@ if(choose == 2)
 			Sleep(10000);
 			system("cls");
 		}
+if(choose == 3)
+		{
+			if(potion >= 1)
+			{
+				system("cls");
+				printf("[포션을 사용하였습니다]\n");
+				
+				Sleep(2000);							
+				hp = 100;
+				mhp = 100;
+			else
+			{
+				system("cls");
+				printf("[포션이 없습니다]\n");
+				Sleep(2000);
+				system("cls");
+			}
+				
+			
