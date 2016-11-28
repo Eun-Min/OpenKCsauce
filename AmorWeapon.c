8d1a95b412a3c0c1bb87097b35b10c32f18bcfd5
popple1101 <popple1101@naver.com>
2016-11-28 20:33:00 +0900

    AmorWeapon

---
+++ AmorWeapon.c
@@ -0,0 +1,3 @@
+ // Amor & Weapon
+ if(weapon == 0){printf("[WEAPON : 맨손]\n");} if(weapon == 1){printf("[WEAPON : 허름한검]\n");}if(weapon == 2){printf("[WEAPON : 쓸만한검]\n");}if(weapon == 3){printf("[WEAPON : 브론즈소드]\n");}
+  if(armor == 0){printf("[ARMOR : 허름한옷]\n\n");}if(armor == 1){printf("[ARMOR : 평상복]\n\n");}if(armor == 2){printf("[ARMOR : 허름한 갑옷]\n\n");}if(armor == 3){printf("[ARMOR : 쓸만한갑옷]\n\n");}
