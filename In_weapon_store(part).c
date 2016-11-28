df2f3b15da80932c4334f500de8d5ffa9fd48a88
Eun-Min <peunmin717@naver.com>
2016-11-28 22:12:16 +0900

    Update In_weapon_store(part).c

--- In_weapon_store(part).c
+++ In_weapon_store(part).c
@@ -1,56 +1,57 @@
-if(menu == 2)
-		{
-			system("cls");
-			Sleep(1000);
-			printf("[무기상점 입장중.]\r");
-			Sleep(1000);
-			printf("[무기상점 입장중..]\r");
-			Sleep(1000);
-			printf("[무기상점 입장중...]\r");
-			Sleep(1000);
+if(menu == 2){
+	system("cls");
+	Sleep(1000);
+	printf("[entering in weapon store.]\r");
+	Sleep(1000);
+	printf("[entering in weapon store..]\r");
+	Sleep(1000);
+	printf("[entering in weapon store...]\r");
+	Sleep(1000);
+		
+	npc1 = rand() % 3+1;
+	if(npc1 == 1){printf("[NPC of weapon : Welcome.]\n");}
+	if(npc1 == 2){printf("[NPC of weapon : I sell only best of best weapons in my store.]\n");}
+	if(npc1 == 3){printf("[NPC of weapon : If you don't have money at all, get out of here.]\n");}
+	printf("[1]허름한검<1레벨> - 1000Gold\n");
+	printf("[2]쓸만한검<3레벨> - 2500Gold\n");
+	printf("[3]브론즈소드<5레벨> - 3800Gold\n");
+	printf("[4]아이언소드<8레벨> - 5200Gold\n"); 
+	printf("[5]아만다디움소드<10레벨> - 7500Gold\n"); 
+	printf("[6]exit\n");
+	scanf("%d",&store1);
 			
-			npc1 = rand() % 3+1;
-			if(npc1 == 1){printf("[무기상인 : 어서오시게나.]\n");} if(npc1 == 2){printf("[무기상인 : 내 가게 무기는 최고의 무기들만 있지]\n");} if(npc1 == 3){printf("[무기상인 : 만약 돈이 없다면 썩 꺼지게나.]\n");}
-			printf("[1]허름한검<1레벨> - 1000골드\n");
-			printf("[2]쓸만한검<3레벨> - 2500골드\n");
-			printf("[3]브론즈소드<5레벨> - 3800골드\n");
-			printf("[4]아이언소드<8레벨> - 5200골드\n"); 
-			printf("[5]아만다디움소드<10레벨> - 7500골드\n"); 
-			printf("[6]나가기\n");
-			scanf("%d",&store1);
-			
-			if(store1 == 1)
-			{
-				if(gold >= 1000)
-				{
-					if(level >= 1)
-					{
-					system("cls");
-					printf("[무기구입중...]\n");
-					Sleep(3000);
-					printf("[무기상인 : 사줘서 고맙네. 잘가게나 친구!]\n");
-					gold -= 1000;
-					str = 15;
-					weapon = 1;
-					Sleep(2000);
-					system("cls");
-					}
-					else
-					{
-						system("cls");
-						printf("[레벨이 맞지 않습니다]\n");
-						Sleep(2000);
-						system("cls");
-					}
-				}
-				else
-				{
-					system("cls");
-					printf("[무기상인 : 돈없으면 이가게에서 당장 나가!]\n");
-					Sleep(2000);
-					system("cls");
-				}
-			}
+	if(store1 == 1)
+	{
+	if(gold >= 1000)
+	{
+	if(level >= 1)
+	{
+	system("cls");
+	printf("[buying weapons...]\n");
+	Sleep(3000);
+	printf("[NPC of weapon : 사줘서 고맙네. 잘가게나 친구!]\n");
+	gold -= 1000;
+	str = 15;
+	weapon = 1;
+	Sleep(2000);
+	system("cls");
+	}
+	else
+	{
+	system("cls");
+	printf("[레벨이 맞지 않습니.]\n");
+	Sleep(2000);
+	system("cls");
+	}
+	}
+	else
+	{
+	system("cls");
+	printf("[NPC of weapon : If you don't have money at all, get out of here!]\n");
+	Sleep(2000);
+	system("cls");
+	}
+	}
 
 			if(store1 == 2)
 			{
@@ -59,9 +60,9 @@ if(menu == 2)
 					if(level >= 3)
 					{
 					system("cls");
-					printf("[무기구입중...]\n");
+					printf("[buying weapons...]\n");
 					Sleep(3000);
-					printf("[무기상인 : 사줘서 고맙네. 잘가게나 친구!]\n");
+					printf("[NPC of weapon : 사줘서 고맙네. 잘가게나 친구!]\n");
 					gold -= 2500;
 					str = 21;
 					weapon = 2;
@@ -79,7 +80,7 @@ if(menu == 2)
 				else
 				{
 					system("cls");
-					printf("[무기상인 : 돈없으면 이가게에서 당장 나가!]\n");
+					printf("[NPC of weapon : If you don't have money at all, get out of here!]\n");
 					Sleep(2000);
 					system("cls");
 				}
@@ -92,9 +93,9 @@ if(menu == 2)
 					if(level >= 5)
 					{
 					system("cls");
-					printf("[무기구입중...]\n");
+					printf("[buying weapons...]\n");
 					Sleep(3000);
-					printf("[무기상인 : 사줘서 고맙네. 잘가게나 친구!]\n");
+					printf("[NPC of weapon : 사줘서 고맙네. 잘가게나 친구!]\n");
 					gold -= 3800;
 					str = 30;
 					weapon = 3;
@@ -112,7 +113,7 @@ if(menu == 2)
 				else
 				{
 					system("cls");
-					printf("[무기상인 : 돈없으면 이가게에서 당장 나가!]\n");
+					printf("[NPC of weapon : If you don't have money at all, get out of here!]\n");
 					Sleep(2000);
 					system("cls");
 				}
@@ -125,9 +126,9 @@ if(menu == 2)
 					if(level >= 8)
 					{
 					system("cls");
-					printf("[무기구입중...]\n");
+					printf("[buying weapons...]\n");
 					Sleep(3000);
-					printf("[무기상인 : 사줘서 고맙네. 잘가게나 친구!]\n");
+					printf("[NPC of weapon : 사줘서 고맙네. 잘가게나 친구!]\n");
 					gold -= 5200;
 					str = 38;
 					weapon = 4;
@@ -145,7 +146,7 @@ if(menu == 2)
 				else
 				{
 					system("cls");
-					printf("[무기상인 : 돈없으면 이가게에서 당장 나가!]\n");
+					printf("[NPC of weapon : If you don't have money at all, get out of here!]\n");
 					Sleep(2000);
 					system("cls");
 				}
@@ -158,9 +159,9 @@ if(menu == 2)
 					if(level >= 10)
 					{
 					system("cls");
-					printf("[무기구입중...]\n");
+					printf("[buying weapons...]\n");
 					Sleep(3000);
-					printf("[무기상인 : 사줘서 고맙네. 잘가게나 친구!]\n");
+					printf("[NPC of weapon : 사줘서 고맙네. 잘가게나 친구!]\n");
 					gold -= 7500;
 					str = 45;
 					weapon = 5;
@@ -178,7 +179,7 @@ if(menu == 2)
 				else
 				{
 					system("cls");
-					printf("[무기상인 : 돈없으면 이가게에서 당장 나가!]\n");
+					printf("[NPC of weapon : If you don't have money at all, get out of here!]\n");
 					Sleep(2000);
 					system("cls");
 				}
@@ -192,7 +193,7 @@ if(menu == 2)
 			if(store1 != 1 && store1 != 2 && store1 != 3 && store1 != 4 && store1 != 5 && store1 != 6)
 			{
 				system("cls");
-				printf("[잘못된 선택입니다]\n");
+				printf("[Wrong Choice.]\n");
 				system("cls");
 			}
 		}
