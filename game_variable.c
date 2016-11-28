8f5c1eb84da9f306e0d65abf65e3e6b2c0f327e4
popple1101 <popple1101@naver.com>
2016-11-28 20:42:17 +0900

    game_variable

---
+++ game_variable.c
@@ -0,0 +1,65 @@
+// ** game basis variable
+
+{
+	int level;
+	int mlevel;
+	int co;
+	int monster;
+	int lev;
+	int hp;
+	int mhp;
+	int exp;
+	int getexp;
+	int defense;
+	int mobdefense;
+	int gold;
+	int getgold;
+	int str;
+	int dex;
+	int store1;
+	int store2;
+	int weapon;
+	int armor;
+	int item;
+	int quest;
+	int q;
+	int starting;
+	int q2;
+	int mobhp;
+	int mobmhp;
+	int menu;
+	int npc1;
+	int npc2;
+	int attack; // 기본공격력
+	int randattack; // 기본공격력 랜덤
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	int g;
+	int h;
+	int i;
+	int choose;
+	int mobattack;
+	int randa; 
+	int potion;
+	int load;
+	int savelevel;
+	int saveexp;
+	int saveweapon;
+	int savearmor;
+	int savegold;
+	int jobs;
+	int job;
+	int skill;
+	int sp;
+	int twoattack;
+	int threeattack;
+	int killattack;
+	int shot;
+    int firebomb;
+    int killing; 
+    int skillc;
+}
