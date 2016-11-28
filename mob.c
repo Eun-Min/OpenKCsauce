24ddf2c9cc3c9c8f0e2ed5a2780cc24774774138
kimDDong <kdh2637@naver.com>
2016-11-28 20:32:44 +0900

    adding potion system by level
    adding potion system by level

--- mob.c
+++ mob.c
@@ -36,16 +36,138 @@ if(choose == 2)
 			Sleep(10000);
 			system("cls");
 		}
-if(choose == 3)
+if(choose == 3)	
 		{
 			if(potion >= 1)
 			{
 				system("cls");
 				printf("[포션을 사용하였습니다]\n");
 				
-				Sleep(2000);							
-				hp = 100;
-				mhp = 100;
+				Sleep(2000);
+
+				if(level == 1)
+				{
+					hp = 100;
+					mhp = 100;
+				}
+            
+				if(level == 2)
+				{
+					hp = 150;
+					mhp = 150;
+				}
+
+				if(level == 3)
+				{
+					hp = 200;
+					mhp = 200;
+				}
+
+				if(level == 4)
+				{
+					hp = 300;
+					mhp = 300;
+				}
+
+				if(level == 5)
+				{
+					hp = 450;
+					mhp = 450;
+				}
+
+				if(level == 6)
+				{
+					hp = 650;
+					mhp = 650;
+				}
+	
+				if(level == 7)
+				{
+					hp = 900;
+					mhp = 900;
+				}
+
+				if(level == 8)
+				{
+					hp = 1200;
+					mhp = 1200;
+				}
+
+				if(level == 9)
+				{
+					hp = 1600;
+					mhp = 1600;
+				}
+
+				if(level == 10)
+				{
+					hp = 2000;
+					mhp = 2000;
+				}
+
+				if(level == 11)
+				{
+					hp = 2500;
+					mhp = 2500;
+				}
+	
+				if(level == 12)
+				{
+					hp = 3000;
+					mhp = 3000;
+				}
+
+				if(level == 13)
+				{
+					hp = 3500;
+					mhp = 3500;
+				}
+
+				if(level == 14)
+				{
+					hp = 4000;
+					mhp = 4000;
+				}
+
+				if(level == 15)
+				{
+					hp = 4500;
+					mhp = 4500;
+				}
+				
+				if(level == 16)
+			{
+				hp = 5000;
+				mhp = 5000;
+			}
+			
+			if(level == 17)
+			{
+				hp = 6000;
+				mhp = 6000;
+			}
+			
+			if(level == 18)
+			{
+				hp = 8000;
+				mhp = 8000;
+			}
+			
+			if(level == 19)
+			{
+				hp = 10000;
+				mhp = 10000;
+			}
+			
+			if(level == 20)
+			{
+				hp = 20000;
+				mhp = 20000;
+			}
+
+				system("cls");
+				potion -= 1;
+			}
 			else
 			{
 				system("cls");
@@ -53,8 +175,8 @@ if(choose == 3)
 				Sleep(2000);
 				system("cls");
 			}
-				
-			
+		}	
+
 if(choose == 4){
 		if(choose == 4)
 		{
