a10cd370599dc9abf6db42bba055e4b26cf3cb2c
Eun-Min <peunmin717@naver.com>
2016-11-28 21:17:35 +0900

    Update AmorWeapon.c

--- AmorWeapon.c
+++ AmorWeapon.c
@@ -1,12 +1,12 @@
 // Amor & Weapon.version1
 //part like weapons of the game
 
-if(weapon == 0){printf("[WEAPON : 맨손]\n");}
-if(weapon == 1){printf("[WEAPON : 허름한검]\n");}
-if(weapon == 2){printf("[WEAPON : 쓸만한검]\n");}
-if(weapon == 3){printf("[WEAPON : 브론즈소드]\n");}
+if(weapon == 0){printf("[WEAPON : hand]\n");}
+if(weapon == 1){printf("[WEAPON : worn sword]\n");}
+if(weapon == 2){printf("[WEAPON : fine-sword]\n");}
+if(weapon == 3){printf("[WEAPON : bronze sword]\n");}
 
-if(armor == 0){printf("[ARMOR : 허름한옷]\n\n");}
-if(armor == 1){printf("[ARMOR : 평상복]\n\n");}
-if(armor == 2){printf("[ARMOR : 허름한 갑옷]\n\n");}
-if(armor == 3){printf("[ARMOR : 쓸만한갑옷]\n\n");}
+if(armor == 0){printf("[ARMOR : shabby clothes]\n\n");}
+if(armor == 1){printf("[ARMOR : ordinary clothes]\n\n");}
+if(armor == 2){printf("[ARMOR : shabby armor]\n\n");}
+if(armor == 3){printf("[ARMOR : fine-armor]\n\n");}
