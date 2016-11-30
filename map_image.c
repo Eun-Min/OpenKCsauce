905b72c6fa2952b13f32f71647afc2f858064184
Eun-Min <peunmin717@naver.com>
2016-12-01 01:02:30 +0900

    Create map_image.c

---
+++ map_image.c
@@ -0,0 +1,15 @@
+int main(int argc, char *argv[]) {
+
+system("mode con:cols=90 lines=50")
+
+char cPosition;
+int iCenterX=7, iCenterY=7;
+gotoXY(iCenterX,  iCenterX);
+printf("*");
+
+gotoXY(3,0);
+printf("\t\t\t텍스트기반맵연습하기\n");
+printf("----------------------------------------------------------------")
+gotoXY(0,30);
+printf("----------------------------------------------------------------")
+}
