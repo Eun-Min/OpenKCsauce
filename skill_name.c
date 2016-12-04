bdd7084f031f89cca02a2cac85b2c32ae4f92476
Eun-Min <peunmin717@naver.com>
2016-12-04 20:56:58 +0900

    Create skill_name.c

---
+++ skill_name.c
@@ -0,0 +1,22 @@
+/* SKILL NAME
+	
+	전사 :
+
+	Heroism -> Onslaught -> Quick Draw -> Frenzy -> Tactics
+	Resistance -> Caution
+	Rage Control -> Training -> Anger -> The power of Darkness
+	
+	마법사 :
+	
+	Wisdom -> Order Magic -> Magic Light -> Mediation -> Concentration
+	Linguistics -> Transmute -> summoner
+	Higher Maigc
+
+	궁수 : 
+
+	Double Strafing -> Arrow Shower -> Charge Arrow -> Phantasmic Arrow
+	-> Ankle Share
+	Machinegun Arrow
+	Create Arrow
+	
+	*/
