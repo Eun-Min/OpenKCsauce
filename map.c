5dc90d65b843dac3a936ad8c7774ead76c4b2339
Eun-Min <peunmin717@naver.com>
2016-12-09 22:38:18 +0900

    Create map.c

---
+++ map.c
@@ -0,0 +1,13 @@
+mapArray[3][16] = {
+111111111111111
+100202120202011
+111111111111111
+}
+for(i=0;i<3;i++) { // column
+for(j=0;j<16;j++) { // ROW
+if(mapArray[i][j] == 0) printf(" ");
+else if(mapArray[i][j] == 1) printf("■");
+else if(mapArray[i][j] == 2) printf("♠");
+else if(mapArray[i][j] == 3) printf(",,");
+}
+}
